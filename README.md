0a3bd0dce18963ee3bb621ba73b419bdd52f4ac4
ふらんちゃん <yuki.suwa.1996@gmail.com>
2015-05-29 00:00:03 +0900

    osu_Downloader
    osu_Downloader

---
+++ README.md
@@ -0,0 +1,37 @@
+﻿osu!Downloader
+===========
+
+##What is it?
+
+osu!の譜面を自動で一括ダウンロードするプログラム。  
+やばそうだったら消します。  
+phpで適当に作ってますが、osu!Notificationのosu_api.rbのget_beatmaps使って作ったほうが詳細に作れたかもしれません。  
+osu!Notificationより先に作っちゃったから仕方ないね。  
+スレッド化してないので譜面が大量にあると結構時間かかります。  
+40譜面x５２ページで８時間ぐらいかかってます。  
+気が向いたらosu_api.rb使ってrubyで作り直します。  
+  
+動作確認はubuntu14.04 PHP 5.5.9-1ubuntu4.5です。
+
+##How to Use
+まずchromeとか使ってリクエストヘッダーの中身からcookieを抜き出してosu-downloader.phpの$cookieに指定してください。  
+次にosuのページから一括でダウンロードしたいものを検索しそのページのURLをosu-downloader.phpの$pageに指定してください。  
+検索結果のページから検索結果が何ページあるかを確認し、$endにそのページ数を指定してください。  
+$startと$endを変更すれば特定のページから特定のページまでをダウンロードするといったこともできます。  
+実行した後./oszの中に譜面データが格納されているはずです。  
+```
+> git clone https://github.com/flum1025/osu_Downloader.git
+> cd osu_Downloader
+> php osu_Downloader.php
+
+```
+
+
+質問等ありましたらTwitter:[@flum_](https://twitter.com/flum_)までお願いします。
+
+##License
+
+The MIT License
+
+-------
+(c) @2015 flum_
---
+++ osu-downloader.php
@@ -0,0 +1,27 @@
+﻿<?php
+$start = 1;  //最初からの場合は1から
+$end = 56; //最後のページを指定する
+$page = 'https://osu.ppy.sh/p/beatmaplist?l=1&r=4&q=4k&g=0&la=0&s=7&o=1&m=3';  //検索したときのURLを貼り付けてください。例はKeywords:4K, mode:osu!mania, Rank status;Allで検索したやつです。
+$cookie = "cookie:";  //chrome等でリクエストヘッダーの中からcookieを抜き出してください。
+
+mkdir($dirpath, 0777);
+for ($start; $start < $end + 1; $start++) {
+echo $start."ページ目\n";
+	$opts = array(
+	  'http'=>array(
+	    'method'=>"GET",
+	    'header'=>"Accept-language: en\r\n" .
+	              $cookie . "\r\n"
+	  )
+	);
+	$context = stream_context_create($opts);
+	$file = file_get_contents($page.'&page='.$start, false, $context);
+	preg_match_all("/<a class='require-login beatmap_download_link' href='\/d\/(\d+)'>/s",$file,$retArr);
+	print_r($retArr[1]);
+
+	for ($i = 0; $i< count($retArr[1]); $i++) {
+		exec("wget -O 'osz/'".$retArr[1][$i].".osz --header '" . $cookie ."' https://osu.ppy.sh/d/" . $retArr[1][$i]);
+		echo $start . "ページ " .$retArr[1][$i] . " is OK.\n";
+	}
+}
+?>
